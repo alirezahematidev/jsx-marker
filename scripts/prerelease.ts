1f593cc2306f578ccebf1bd6669505007ac3e961
alirezahematidev <alirezahematideveloper71@gmail.com>
2024-04-05 21:45:55 +0330

    sleep fn

--- scripts/prerelease.ts
+++ scripts/prerelease.ts
@@ -2,7 +2,11 @@ import fs from "node:fs/promises";
 
 import glob from "fast-glob";
 
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
+
 async function copy() {
+  await sleep();
+
   const dts = await glob("dist/**/*.d.ts", { absolute: true });
 
   await Promise.all(dts.map((file) => fs.copyFile(file, file.replace(/\.d\.ts$/, ".d.cts"))));
